d1789ffbbc2ab0d76182463e24f2f3343fde3cc2
nur0izzati <izzati0zumaini@gmail.com>
2026-05-14 22:44:50 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,21 @@
-const CACHE_NAME = 'fitquest-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './game.js',
-  './manifest.json',
-  'https://cdn.jsdelivr.net/npm/phaser@3.60.0/dist/phaser.min.js'
+const CACHE_NAME = "fitquest-v1";
+
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/manifest.json"
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
